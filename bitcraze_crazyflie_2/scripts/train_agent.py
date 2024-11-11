eb60eeb296246b4df554f5c50b0665f17448432a
Viktor Lorentz <s78358@bht-berlin.de>
2024-11-11 22:05:01 +0100

    good result

--- bitcraze_crazyflie_2/scripts/train_agent.py
+++ bitcraze_crazyflie_2/scripts/train_agent.py
@@ -20,24 +20,24 @@ def main():
     env_id = "DroneEnv-v0"
 
     # Define parameters
-    num_envs = 16  
+    num_envs = 8  
     n_steps = 512  
     batch_size = 512  # Should be a factor of total_timesteps_per_update
-    time_steps = 2_000_000  # Total training timesteps
+    time_steps = 1_000_000  # Total training timesteps
 
     # Reward function coefficients
     reward_coefficients = {
-        "distance_z": 1.0,
-        "distance_xy": 1.0,
-        "rotation_penalty": 2.0,
+        "distance_z": 0.0,
+        "distance_xy": 0.0,
+        "rotation_penalty": 3.0,
         "z_angular_velocity": 0.2,
         "angular_velocity": 0.01,
         "collision_penalty": 10.0,
         "out_of_bounds_penalty": 10.0,
-        "alive_reward": 1.0,
-        "linear_velocity": 0.1,
-        "goal_bonus": 10.0,
-        "distance" : 0
+        "alive_reward": 3.0,
+        "linear_velocity": 0.5,
+        "goal_bonus": 20.0,
+        "distance" : 2.0
     }
 
     # Config for wandb (include important parameters for sweeps)
