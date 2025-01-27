3e028bf9079e47ac7c113fd77ea067042c884541
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-01-27 20:08:42 +0100

    adjust alive reward

--- bitcraze_crazyflie_2/scripts/train_agent.py
+++ bitcraze_crazyflie_2/scripts/train_agent.py
@@ -146,7 +146,7 @@ def main():
         "distance_z": 0,
         "goal_bonus": 20,
         "distance_xy": 0,
-        "alive_reward": 5,
+        "alive_reward": 8,
         "linear_velocity": 0,
         "angular_velocity": 0,
         "rotation_penalty": 1,
