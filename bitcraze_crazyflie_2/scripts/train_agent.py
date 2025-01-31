95aac1632f757cbcd0e4a000c57257ac5a426d61
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-01-31 10:48:56 +0100

    try better params

--- bitcraze_crazyflie_2/scripts/train_agent.py
+++ bitcraze_crazyflie_2/scripts/train_agent.py
@@ -142,11 +142,11 @@ def main():
 
     # Reward function coefficients
     reward_coefficients = {  # based on single_quad_rl_1731931528
-        "distance": 1,
+        "distance": 2,
         "distance_z": 0,
         "goal_bonus": 100,
         "distance_xy": 0,
-        "alive_reward": 8,
+        "alive_reward": 5,
         "linear_velocity": 0,
         "angular_velocity": 0,
         "rotation_penalty": 1,
@@ -154,9 +154,9 @@ def main():
         "z_angular_velocity": 1,
         "terminate_collision": True,
         "out_of_bounds_penalty": 0,
-        "velocity_towards_target": 1,
+        "velocity_towards_target": 0.5,
         "action_saturation": 0,
-        "smooth_action": 0.2,
+        "smooth_action": 0.5,
         "energy_penalty": 0.1,
         "payload_velocity": 0.05,
         "above_payload": 0.2,
