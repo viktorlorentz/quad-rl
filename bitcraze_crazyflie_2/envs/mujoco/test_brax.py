d1c7dec3362fae1099e140892ec5b7b38ade3261
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-03-05 11:03:08 +0100

    lower dis reward

--- bitcraze_crazyflie_2/envs/mujoco/test_brax.py
+++ bitcraze_crazyflie_2/envs/mujoco/test_brax.py
@@ -296,7 +296,7 @@ class MultiQuadEnv(PipelineEnv):
 
     # Combine components to form the final reward.
     reward = 0
-    reward += 10 * distance_reward
+    reward += 2 * distance_reward
     reward += safe_distance_reward
     # reward += velocity_towards_target
     #reward += quad_above_reward
@@ -355,7 +355,7 @@ train_fn = functools.partial(
     unroll_length=20,              # Collect sequences of 10 timesteps per rollout to capture short-term dynamics.
     num_minibatches=32,            # Split the full batch into 32 minibatches to help stabilize the gradient updates.
     num_updates_per_batch=4,       # Apply 4 SGD updates per batch of data.
-    discounting=0.995,              # Standard discount factor to balance immediate and future rewards.
+    discounting=0.98,              # Standard discount factor to balance immediate and future rewards.
     learning_rate=3e-4,            # A common starting learning rate that works well in many Brax tasks.
     entropy_cost=1e-2,             # Encourage exploration with a modest entropy bonus.
     num_envs=2048,                 # Run 2048 parallel environment instances for efficient data collection.
