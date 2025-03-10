03a88608c7d6b34bd5a8db118406c5b85bec42e7
Viktor Lorentz <viktor.lorentz@t-online.de>
2025-03-11 00:05:48 +0100

    correct import

--- bitcraze_crazyflie_2/envs/mujoco/test_mava.py
+++ bitcraze_crazyflie_2/envs/mujoco/test_mava.py
@@ -324,8 +324,8 @@ def make_multiagent_env(config):
 
 def main():
     # Import Mava experiment runner and configuration helper.
-    from mava import run_experiment
-    from mava.systems.ppo.anakin.ff_mappo import main as run_experiment
+    from mava.systems.ppo.anakin.ff_ippo import main as run_experiment
+    from mava.utils.configurations import get_default_config
 
     # Get default configuration for decentralized MAPPO.
     config = get_default_config("mappo_decentralized")
